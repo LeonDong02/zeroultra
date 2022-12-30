2cd12a9955c999632717fd9a8930018ff6fa24c5
Leon Dong <leon.d1043@gmail.com>
2022-12-30 00:19:39 -0500

    yea

---
+++ pages/components/links.js
@@ -0,0 +1,15 @@
+export default function Links() {
+    return (
+        <div class="links">
+            <a href="https://github.com/LeonDong02" target="_blank">
+                <img src={'icons8-github.svg'} class="icon" />
+            </a>
+            <a href="https://www.linkedin.com/in/leondong02/" target="_blank">
+                <img src={'icons8-linkedin.svg'} class="icon" />
+            </a>
+            <a href="https://twitter.com/wetpeeker" target="_blank">
+                <img src={'icons8-twitter.svg'} class="icon" />
+            </a>
+        </div>
+    )
+}
