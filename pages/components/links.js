8f78ed17f8f55a850180756be0c2013120e74fd5
Leon Dong <leon.d1043@gmail.com>
2022-12-30 00:24:29 -0500

    refer

--- pages/components/links.js
+++ pages/components/links.js
@@ -1,13 +1,13 @@
 export default function Links() {
     return (
         <div class="links">
-            <a href="https://github.com/LeonDong02" target="_blank" rel="noopener">
+            <a href="https://github.com/LeonDong02" target="_blank" rel="noreferrer">
                 <img src={'icons8-github.svg'} class="icon" />
             </a>
-            <a href="https://www.linkedin.com/in/leondong02/" target="_blank" rel="noopener">
+            <a href="https://www.linkedin.com/in/leondong02/" target="_blank" rel="noreferrer">
                 <img src={'icons8-linkedin.svg'} class="icon" />
             </a>
-            <a href="https://twitter.com/wetpeeker" target="_blank" rel="noopener">
+            <a href="https://twitter.com/wetpeeker" target="_blank" rel="noreferrer">
                 <img src={'icons8-twitter.svg'} class="icon" />
             </a>
         </div>
