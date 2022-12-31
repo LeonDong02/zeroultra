70b0fe5bb1eec25175dc8d2367c5c39983f431f3
Leon Dong <leon.d1043@gmail.com>
2022-12-31 01:35:11 -0500

    Update now-playing.js

--- pages/api/now-playing.js
+++ pages/api/now-playing.js
@@ -3,59 +3,63 @@ const { SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, SPOTIFY_REFRESH_TOKEN } =
     process.env;
 
 const getAccessToken = async () => {
-    const response = await fetch("https://accounts.spotify.com/api/token", {
-        method: "POST",
-        headers: {
-            Authorization: `Basic ${Buffer.from(
-                `${SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`
-            ).toString("base64")}`,
-            "Content-Type": "application/x-www-form-urlencoded",
-        },
-        body: new URLSearchParams({
-            grant_type: "refresh_token",
-            refresh_token: SPOTIFY_REFRESH_TOKEN ?? "",
-        }),
-    });
+    try {
+        const response = await fetch("https://accounts.spotify.com/api/token", {
+            method: "POST",
+            headers: {
+                Authorization: `Basic ${Buffer.from(
+                    `${SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`
+                ).toString("base64")}`,
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body: new URLSearchParams({
+                grant_type: "refresh_token",
+                refresh_token: SPOTIFY_REFRESH_TOKEN ?? "",
+            }),
+        });
 
-    return await response.json();
+        return await response.json();
+    } catch (e) {
+        return e;
+    }
 };
 
 export const currentlyPlayingSong = async () => {
-    const { access_token } = await getAccessToken();
-    console.error("Access token: ", access_token);
+    try {
+        const { access_token } = await getAccessToken();
 
-    const response = await fetch(
-        "https://api.spotify.com/v1/me/player/currently-playing",
-        {
-            headers: {
-                Authorization: `Bearer ${access_token}`,
-            },
-        }
-    );
+        const response = await fetch(
+            "https://api.spotify.com/v1/me/player/currently-playing",
+            {
+                headers: {
+                    Authorization: `Bearer ${access_token}`,
+                },
+            }
+        );
 
-    return await response.json();
+        return await response.json();
+    } catch (e) {
+        return e;
+    }
 };
 
 export default async function handler(req, res) {
-    const song = await currentlyPlayingSong();
-    console.error("Song: ", song);
+    try {
+        const song = await currentlyPlayingSong();
 
-    if (
-        !song?.item ||
-        Object.keys(song.item).length === 0 ||
-        !song.is_playing
-    ) {
-        return res
-            .status(200)
-            .json({ isPlaying: false, data: JSON.stringify(song) });
+        if (!song?.item || !song.is_playing) {
+            throw new Error();
+        }
+
+        return res.status(200).json({
+            album: song.item.album.name,
+            albumImageUrl: song.item.album.images[0].url,
+            artist: song.item.artists.map((_artist) => _artist.name).join(", "),
+            isPlaying: song.is_playing,
+            songUrl: song.item.external_urls.spotify,
+            title: song.item.name,
+        });
+    } catch (e) {
+        return res.status(200).json({ isPlaying: false });
     }
-
-    return res.status(200).json({
-        album: song.item.album.name,
-        albumImageUrl: song.item.album.images[0].url,
-        artist: song.item.artists.map((_artist) => _artist.name).join(", "),
-        isPlaying: song.is_playing,
-        songUrl: song.item.external_urls.spotify,
-        title: song.item.name,
-    });
 }
