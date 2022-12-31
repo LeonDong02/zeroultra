b10583f0d8152edd4a434fce3fce2aea4eaceb6c
Leon Dong <leon.d1043@gmail.com>
2022-12-31 00:25:08 -0500

    Update now-playing.js

--- pages/api/now-playing.js
+++ pages/api/now-playing.js
@@ -1,5 +1,3 @@
-import { currentlyPlayingSong } from "../../scripts/spotify";
-
 const client_id = process.env.SPOTIFY_CLIENT_ID;
 const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
 const refresh_token = process.env.SPOTIFY_REFRESH_TOKEN;
