6ff040932e2b35077ce2fff874771e88dd97bde8
Leon Dong <leon.d1043@gmail.com>
2022-12-31 00:41:12 -0500

    Update now-playing.js

--- pages/api/now-playing.js
+++ pages/api/now-playing.js
@@ -1,48 +1,64 @@
-const client_id = process.env.SPOTIFY_CLIENT_ID;
-const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
-const refresh_token = process.env.SPOTIFY_REFRESH_TOKEN;
+const {
+    SPOTIFY_CLIENT_ID: client_id,
+    SPOTIFY_CLIENT_SECRET: client_secret,
+    SPOTIFY_REFRESH_TOKEN: refresh_token,
+} = process.env;
 
 const getAccessToken = async () => {
-    const refresh_token = process.env.SPOTIFY_REFRESH_TOKEN;
-  
-    const response = await fetch("https://accounts.spotify.com/api/token", {
-        method: "POST",
-        headers: {
-            Authorization: `Basic ${Buffer.from(
-            `${client_id}:${client_secret}`
-            ).toString("base64")}`,
-            "Content-Type": "application/x-www-form-urlencoded",
-        },
-        body: new URLSearchParams({
-            grant_type: "refresh_token",
-            refresh_token,
-        }),
-    });
-  
-    return response.json();
-};
+    try {
+        const response = await fetch("https://accounts.spotify.com/api/token", {
+            method: "POST",
+            headers: {
+                Authorization: `Basic ${Buffer.from(
+                    `${client_id}:${client_secret}`
+                ).toString("base64")}`,
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body: new URLSearchParams({
+                grant_type: "refresh_token",
+                refresh_token,
+            }),
+        });
 
+        console.log("Env vars: ", client_id, client_secret, refresh_token);
+        console.log("Access token resp: ", response);
+
+        return await response.json();
+    } catch (e) {
+        console.error(e);
+    }
+};
 
 export const currentlyPlayingSong = async () => {
     const { access_token } = await getAccessToken();
-  
-    return fetch("https://api.spotify.com/v1/me/player/currently-playing", {
-        headers: {
-            Authorization: `Bearer ${access_token}`,
-        },
-    });
+    console.log("Token: ", access_token);
+
+    try {
+        const response = await fetch(
+            "https://api.spotify.com/v1/me/player/currently-playing",
+            {
+                headers: {
+                    Authorization: `Bearer ${access_token}`,
+                },
+            }
+        );
+
+        return await response.json();
+    } catch (e) {
+        console.error(e);
+    }
 };
 
 export default async function handler(req, res) {
-    const response = await currentlyPlayingSong();
+    let song;
+    try {
+        song = await currentlyPlayingSong();
+        console.log("Song: ", song);
 
-    if (response.status === 204 || response.status > 400) {
-        return res.status(200).json({ isPlaying: false });
-    }
-
-    const song = await response.json();
-
-    if (song.item === null) {
+        if (!song.item || !song.is_playing) {
+            return new Error();
+        }
+    } catch {
         return res.status(200).json({ isPlaying: false });
     }
 
