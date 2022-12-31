5b01e743e6b1346da803752024ec0f059bd764cc
Leon Dong <leon.d1043@gmail.com>
2022-12-31 02:20:56 -0500

    Update spotify.js

--- pages/components/spotify.js
+++ pages/components/spotify.js
@@ -1,27 +1,104 @@
 import useSWR from "swr";
+import { useEffect } from "react";
+import { animate } from "motion";
 
 const fetcher = url => fetch(url).then(res => res.json())
+
+function AnimatedBars() {
+    useEffect(() => {
+        animate(
+            "#bar1",
+            {
+            transform: [
+                "scaleY(1.0) translateY(0rem)",
+                "scaleY(1.5) translateY(-0.082rem)",
+                "scaleY(1.0) translateY(0rem)",
+            ],
+            },
+            {
+            duration: 1.0,
+            repeat: Infinity,
+            easing: ["ease-in-out"],
+            }
+        );
+        animate(
+            "#bar2",
+            {
+            transform: [
+                "scaleY(1.0) translateY(0rem)",
+                "scaleY(3) translateY(-0.083rem)",
+                "scaleY(1.0) translateY(0rem)",
+            ],
+            },
+            {
+            delay: 0.2,
+            duration: 1.5,
+            repeat: Infinity,
+            easing: ["ease-in-out"],
+            }
+        );
+        animate(
+            "#bar3",
+            {
+            transform: [
+                "scaleY(1.0)  translateY(0rem)",
+                "scaleY(0.5) translateY(0.37rem)",
+                "scaleY(1.0)  translateY(0rem)",
+            ],
+            },
+            {
+            delay: 0.3,
+            duration: 1.5,
+            repeat: Infinity,
+            easing: ["ease-in-out"],
+            }
+        );
+    }, []);
+    
+    return (
+        <div className="w-auto flex items-end overflow-hidden">
+            <span
+            id="bar1"
+            className="w-1 mr-[3px] h-2 bg-gray-300 dark:bg-gray-500 opacity-75"
+            />
+            <span
+            id="bar2"
+            className="w-1 mr-[3px] h-1 bg-gray-300 dark:bg-gray-500"
+            />
+            <span
+            id="bar3"
+            className="w-1 h-3 bg-gray-300 dark:bg-gray-500 opacity-80"
+            />
+        </div>
+    );
+}
 
 export default function NowPlaying() {
   const { data } = useSWR("/api/now-playing", fetcher);
 
   return (
     <div>
-      <div>
-        {data?.songUrl && data?.isPlaying ? (
-          <a href={data.songUrl} target="_blank" rel="noopener noreferrer">
-            <u>
-                {data.title + " - " + data?.artist}
-            </u>
-          </a>
-        ) : (
-          <a href="https://open.spotify.com/user/22offyqbuyz7qehcrv7sssa4y?si=6a8ebdaecab045e3" target="_blank" rel="noopener noreferrer">
-            <u>
-                Not currently listening
-            </u>
-          </a>
-        )}
-      </div>
+        <div>
+            <nobr>
+                {data?.songUrl && data?.isPlaying ? (
+                    <div>
+                        Now playing:&nbsp;
+                        <a href={data.songUrl} target="_blank" rel="noopener noreferrer">
+                            <u>
+                                {data.title + " - " + data?.artist}
+                            </u>
+                        </a>
+                    </div>
+                ) : (
+                    <a href="https://open.spotify.com/user/22offyqbuyz7qehcrv7sssa4y?si=6a8ebdaecab045e3" target="_blank" rel="noopener noreferrer">
+                        Now playing:&nbsp;
+                        <u>
+                            Not currently listening
+                        </u>
+                    </a>
+                )}
+            </nobr>
+        </div>
     </div>
   );
 }
