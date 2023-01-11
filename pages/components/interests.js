d4b27dfc9f5ee444bc352162532570078258ee39
Leon Dong <leon.d1043@gmail.com>
2023-01-11 16:18:44 -0500

    dddddd

---
+++ pages/components/interests.js
@@ -0,0 +1,71 @@
+export default function Others() {
+    return (
+        <div>
+            <div class="text-xl font-bold">
+                interests
+            </div>
+            <div class="flex flex-col md:flex-row mb-2.5">
+                <div class="basis-1/2">
+                    <ul>
+                        <li class="mb-1">
+                            agi/ml <br></br>[
+                            <a href='https://www.coherentapp.tech/' target="_blank" rel="noreferrer" class="transition ease-in-out underline text-violet-700 hover:text-violet-900 duration-100">
+                            coherent
+                            </a>
+                            /
+                            <a href='https://github.com/LeonDong02/pennappsxx' target="_blank" rel="noreferrer" class="transition ease-in-out underline text-violet-700 hover:text-violet-900 duration-100">
+                            moneymachine
+                            </a>
+                            /
+                            <a href='https://uwflow.com/course/cs486' target="_blank" rel="noreferrer" class="transition ease-in-out underline text-violet-700 hover:text-violet-900 duration-100">
+                            cs486
+                            </a>
+                            ]
+                        </li>
+                        <li class="mb-1">
+                            security/cryptography <br></br>[
+                            <a href='https://picoctf.org/' target="_blank" rel="noreferrer" class="transition ease-in-out underline text-violet-700 hover:text-violet-900 duration-100">
+                            picoctf
+                            </a>
+                            /
+                            <a href='https://ctf.issessions.ca/' target="_blank" rel="noreferrer" class="transition ease-in-out underline text-violet-700 hover:text-violet-900 duration-100">
+                            issessions
+                            </a>
+                            /
+                            <a href='https://uwflow.com/course/co487' target="_blank" rel="noreferrer" class="transition ease-in-out underline text-violet-700 hover:text-violet-900 duration-100">
+                            co487
+                            </a>
+                            /
+                            <a href='https://uwflow.com/course/cs458' target="_blank" rel="noreferrer" class="transition ease-in-out underline text-violet-700 hover:text-violet-900 duration-100">
+                            cs458
+                            </a>
+                            ]
+                        </li>
+                    </ul>
+                </div>
+                <div class="basis-1/2">
+                    <ul>
+                        <li class="mb-1">
+                            web3 <br></br>[
+                            <a href='https://github.com/LeonDong02/habipets' target="_blank" rel="noreferrer" class="transition ease-in-out underline text-violet-700 hover:text-violet-900 duration-100">
+                            habipets
+                            </a>
+                            /
+                            <a href='https://twitter.com/uw_blockchain' target="_blank" rel="noreferrer" class="transition ease-in-out underline text-violet-700 hover:text-violet-900 duration-100">
+                            uwblockchain
+                            </a>
+                            ]
+                        </li>
+                        <li class="mb-1">
+                            quantum computing <br></br>[
+                            <a href='https://uwflow.com/course/co481' target="_blank" rel="noreferrer" class="transition ease-in-out underline text-violet-700 hover:text-violet-900 duration-100">
+                            co481
+                            </a>
+                            ]
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ pages/components/others.js
@@ -0,0 +1,87 @@
+export default function Others() {
+    return (
+        <div>
+            <div class="text-xl font-bold">
+                other cool stuff
+            </div>
+            <div class="flex flex-col md:flex-row">
+                <div class="basis-1/2">
+                    <ul>
+                        <li class="mb-1">
+                            projects <br></br>[
+                            <a href='https://github.com/LeonDong02' target="_blank" rel="noreferrer" class="transition ease-in-out underline text-violet-700 hover:text-violet-900 duration-100">
+                            github
+                            </a>
+                            /
+                            <a href='https://devpost.com/LeonDong' target="_blank" rel="noreferrer" class="transition ease-in-out underline text-violet-700 hover:text-violet-900 duration-100">
+                            devpost
+                            </a>
+                            ]
+                        </li>
+                        <li class="mb-1">
+                            competitive programming <br></br>[
+                            <a href='https://codeforces.com/profile/p1geon' target="_blank" rel="noreferrer" class="transition ease-in-out underline text-violet-700 hover:text-violet-900 duration-100">
+                            cf
+                            </a>
+                            /
+                            <a href='https://dmoj.ca/user/p1geon' target="_blank" rel="noreferrer" class="transition ease-in-out underline text-violet-700 hover:text-violet-900 duration-100">
+                            dmoj
+                            </a>
+                            ]
+                        </li>
+                    </ul>
+                </div>
+                <div class="basis-1/2">
+                    <ul>
+                        <li class="mb-1">
+                            music <br></br>[
+                            <a href='https://open.spotify.com/user/22offyqbuyz7qehcrv7sssa4y?si=273a075fb0a049b3' target="_blank" rel="noreferrer" class="transition ease-in-out underline text-violet-700 hover:text-violet-900 duration-100">
+                            spotify
+                            </a>
+                            /
+                            <a href='https://www.last.fm/user/p1geon_' target="_blank" rel="noreferrer" class="transition ease-in-out underline text-violet-700 hover:text-violet-900 duration-100">
+                            lastfm
+                            </a>
+                            ]
+                        </li>
+                        <li class="mb-1">
+                            valorant <br></br>[
+                            <a href='https://www.youtube.com/watch?v=zf37WxQCW68' target="_blank" rel="noreferrer" class="transition ease-in-out underline text-violet-700 hover:text-violet-900 duration-100">
+                            montage
+                            </a>
+                            /
+                            <a href='https://www.youtube.com/@wetpeeker' target="_blank" rel="noreferrer" class="transition ease-in-out underline text-violet-700 hover:text-violet-900 duration-100">
+                            channel
+                            </a>
+                            /
+                            <a href='https://tracker.gg/valorant/profile/riot/mc2van%23na1/overview' target="_blank" rel="noreferrer" class="transition ease-in-out underline text-violet-700 hover:text-violet-900 duration-100">
+                            tracker
+                            </a>
+                            ]
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="mb-2.5">
+                <ul>
+                    <li>
+                        contact <br></br>[
+                        <a href='https://www.linkedin.com/in/leondong02/' target="_blank" rel="noreferrer" class="transition ease-in-out underline text-violet-700 hover:text-violet-900 duration-100">
+                        linkedin
+                        </a>
+                        /
+                        <a href='https://twitter.com/wetpeeker' target="_blank" rel="noreferrer" class="transition ease-in-out underline text-violet-700 hover:text-violet-900 duration-100">
+                        twitter
+                        </a>
+                        /
+                        <a href='mailto:yiming.dong@uwaterloo.ca' target="_blank" rel="noreferrer" class="transition ease-in-out underline text-violet-700 hover:text-violet-900 duration-100">
+                        email
+                        </a>
+                        ]
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
