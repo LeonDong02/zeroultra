3ad7899339980e15696c271992f870812831a05e
Leon Dong <leon.d1043@gmail.com>
2023-01-26 12:53:33 -0500

    dfdfd

--- pages/components/construction.js
+++ pages/components/construction.js
@@ -1,17 +1,17 @@
 export default function Construction() {
     return (
         <div class="construction-border">
-            <br></br>
+            <br class="seperator"></br>
             <div class="construction text-violet-500">under construction</div>
-            <br></br>
+            <br class="seperator"></br>
             <div class="construction">
                 d3l-n3st webring
             </div>
-            <br></br>
+            <br class="seperator"></br>
             <div class="construction">
                 right-brain website
             </div>
-            <br></br>
+            <br class="seperator"></br>
         </div>
     )
 }
