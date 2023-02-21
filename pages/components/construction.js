d5b66dc5b440a906190a14ad79023301e7f8435a
Leon Dong <leon.d1043@gmail.com>
2023-02-20 23:57:10 -0500

    webring

--- pages/components/construction.js
+++ pages/components/construction.js
@@ -2,19 +2,21 @@ export default function Construction() {
     return (
         <div class="construction-border">
             <br class="seperator"></br>
-            <div class="construction">under construction</div>
+            <div class="center">under construction</div>
             <br class="seperator"></br>
-            <div class="construction text-violet-500">
-                d3l-n3st webring
+            <div class="center text-violet-500">
+                <div class="center">
+                    <a href="https://d3l-n3st.vercel.app/prev" class="transition ease-in-out underline text-violet-500 hover:text-violet-700 duration-100">&lt;&lt;</a>//<a href="https://d3l-n3st.vercel.app/" class="transition ease-in-out underline text-violet-500 hover:text-violet-700 duration-100">d3l n3st webring</a>//<a href="https://d3l-n3st.vercel.app/next" class="transition ease-in-out underline text-violet-500 hover:text-violet-700 duration-100">&gt;&gt;</a>
+                </div>
             </div>
             <br class="seperator"></br>
-            <div class="construction">
+            <div class="center">
                 <a href='https://zeroultra.neocities.org/' target="_blank" rel="noreferrer" class="transition ease-in-out underline text-violet-500 hover:text-violet-700 duration-100">
                     right-brain website
                 </a>
             </div>
             <br class="seperator"></br>
-            <div class="construction text-violet-500">
+            <div class="center text-violet-500">
                 &ldquo;starry night&rdquo;
             </div>
             <br class="seperator"></br>
