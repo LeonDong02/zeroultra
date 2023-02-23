2f16e3b07a9f7b6c97b04a65f6e22f7204345143
Leon Dong <leon.d1043@gmail.com>
2023-02-23 03:50:18 -0500

    Update construction.js

--- pages/components/construction.js
+++ pages/components/construction.js
@@ -8,7 +8,7 @@ export default function Construction() {
                 <span class="highlight">
                     <a href="https://d3l-n3st.vercel.app/prev" class="transition ease-in-out underline text-violet-500 hover:text-violet-700 duration-100">&lt;&lt;</a>
                     &nbsp;
-                    <a href="https://d3l-n3st.vercel.app/" class="transition ease-in-out underline text-violet-500 hover:text-violet-700 duration-100">✧d3l-n3st✧</a>
+                    <a href="https://d3l-n3st.vercel.app/" class="transition ease-in-out underline text-violet-500 hover:text-violet-700 duration-100">d3l✧n3st</a>
                     &nbsp;
                     <a href="https://d3l-n3st.vercel.app/next" class="transition ease-in-out underline text-violet-500 hover:text-violet-700 duration-100">&gt;&gt;</a>
                 </span>
