09e292349cdc9787f9d5de73768c8b6496121daa
Leon Dong <leon.d1043@gmail.com>
2023-02-22 19:30:21 -0500

    Update construction.js

--- pages/components/construction.js
+++ pages/components/construction.js
@@ -1,25 +1,27 @@
 export default function Construction() {
     return (
         <div class="construction-border">
-            <span class="highlight">
-                <br class="seperator"></br>
-                <div class="center">under construction</div>
-                <br class="seperator"></br>
-                <div class="center">
+            <br class="seperator"></br>
+            <div class="center">under construction</div>
+            <br class="seperator"></br>
+            <div class="center">
+                <span class="highlight">
                     <a href="https://d3l-n3st.vercel.app/prev" class="transition ease-in-out underline text-violet-500 hover:text-violet-700 duration-100">&lt;&lt;</a>
                     &nbsp;✧&nbsp;
                     <a href="https://d3l-n3st.vercel.app/" class="transition ease-in-out underline text-violet-500 hover:text-violet-700 duration-100">d3l-n3st</a>
                     &nbsp;✧&nbsp;
                     <a href="https://d3l-n3st.vercel.app/next" class="transition ease-in-out underline text-violet-500 hover:text-violet-700 duration-100">&gt;&gt;</a>
-                </div>
-                <br class="seperator"></br>
-                <div class="center">
+                </span>
+            </div>
+            <br class="seperator"></br>
+            <div class="center">
+                <span class="highlight">
                     <a href='https://zeroultra.neocities.org/' target="_blank" rel="noreferrer" class="transition ease-in-out underline text-violet-500 hover:text-violet-700 duration-100">
                         right-brain website
                     </a>
-                </div>
-                <br class="seperator"></br>
-            </span>
+                </span>
+            </div>
+            <br class="seperator"></br>
         </div>
     )
 }
