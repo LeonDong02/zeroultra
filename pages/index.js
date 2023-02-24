a4594c438e5e1184410bf210cdf2e2d4af914de2
Leon Dong <leon.d1043@gmail.com>
2023-02-24 00:55:28 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -157,9 +157,9 @@ export default function Home() {
     if (!played) {
       var music = new Audio('SwedenC418.mp3');
       var crickets = new Audio('crickets.mp3');
-      music.volume = 0.02;
+      music.volume = 0.03;
       music.loop = true;
-      crickets.volume = 0.03;
+      crickets.volume = 0.045;
       crickets.loop = true;
       music.play();
       crickets.play();
