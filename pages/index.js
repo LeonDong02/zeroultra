d20873c630cac63c3793ff929544490147a13943
Leon Dong <leon.d1043@gmail.com>
2023-02-25 04:06:45 -0500

    better crickets and louder stuff

--- pages/index.js
+++ pages/index.js
@@ -157,9 +157,9 @@ export default function Home() {
     if (!played) {
       var music = new Audio('SwedenC418.mp3');
       var crickets = new Audio('crickets.mp3');
-      music.volume = 0.08;
+      music.volume = 0.12;
       music.loop = true;
-      crickets.volume = 0.12;
+      crickets.volume = 0.18;
       crickets.loop = true;
       music.play();
       crickets.play();
